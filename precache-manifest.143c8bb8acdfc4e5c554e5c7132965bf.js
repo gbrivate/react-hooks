5ea70dc455a9db9ddc95891b94d57ffdc292453e
Gabriel Santos <gabrielinflex@gmail.com>
2020-04-16 23:15:50 -0300

    Updates

---
+++ precache-manifest.143c8bb8acdfc4e5c554e5c7132965bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b37d6cf93e512f19498f8470a551f4",
+    "url": "/react-hooks/index.html"
+  },
+  {
+    "revision": "69a3872fd0ac9bb12138",
+    "url": "/react-hooks/static/js/2.76d8c512.chunk.js"
+  },
+  {
+    "revision": "fb53afac904a19f752339ed5143ffede",
+    "url": "/react-hooks/static/js/2.76d8c512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de15e9e4ab68610f9351",
+    "url": "/react-hooks/static/js/main.3c72859a.chunk.js"
+  },
+  {
+    "revision": "fb019f219914f2a8ea2b",
+    "url": "/react-hooks/static/js/runtime-main.3818444d.js"
+  }
+]);
